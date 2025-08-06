26482989848aef2bb44c105181a938e67bbf4a2c
kayleevanaalst <vaka23lt@student.ju.se>
2025-08-06 20:04:05 +0200

    change

--- activities.js
+++ activities.js
@@ -22,7 +22,7 @@ function renderProducts(list) {
 
       <div style="display: flex; justify-content: space-between; align-items: center; margin-top: 10px">
         <p style="margin: 8px 0;"><strong>${p.price} SEK</strong></p>
-        <a href="details.html?id=${p.id}" class="book-btn">More Info</a>
+       <a href="/web-design-project-group-21/details.html?id=${p.id}" class="book-btn">More Info</a>
       </div>
     `;
     container.appendChild(card);
